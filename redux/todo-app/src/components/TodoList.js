4742bc49fbd088b03d3e019cc7ea5409a4a1ed0c
Eda Dural <edaadurall@gmail.com>
2025-06-17 16:22:34 +0300

    destroy

--- redux/todo-app/src/components/TodoList.js
+++ redux/todo-app/src/components/TodoList.js
@@ -1,9 +1,15 @@
 import { useDispatch, useSelector } from "react-redux";
-import { toggle } from "../redux/todos/todosSlice";
+import { toggle, destroy } from "../redux/todos/todosSlice";
 
 function TodoList() {
   const dispatch = useDispatch();
   const todos = useSelector((state) => state.todos.items);
+
+  const handleDestroy = (id) => {
+    if (window.confirm("Are you sure?")) {
+      dispatch(destroy(id));
+    }
+  };
 
   return (
     <ul className="todo-list">
@@ -17,7 +23,10 @@ function TodoList() {
               onChange={() => dispatch(toggle({ id: item.id }))}
             />
             <label>{item.title}</label>
-            <button className="destroy"></button>
+            <button
+              className="destroy"
+              onClick={() => handleDestroy(item.id)}
+            ></button>
           </div>
         </li>
       ))}
