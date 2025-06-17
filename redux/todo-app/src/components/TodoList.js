d78ba0d28544711fc49758865327378f9ebdb4d6
Eda Dural <edaadurall@gmail.com>
2025-06-17 15:34:47 +0300

    toggle

--- redux/todo-app/src/components/TodoList.js
+++ redux/todo-app/src/components/TodoList.js
@@ -1,15 +1,21 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { toggle } from "../redux/todos/todosSlice";
 
 function TodoList() {
+  const dispatch = useDispatch();
   const todos = useSelector((state) => state.todos.items);
-  console.log(todos);
 
   return (
     <ul className="todo-list">
       {todos.map((item) => (
         <li key={item.id} className={item.completed ? "completed" : ""}>
           <div className="view">
-            <input className="toggle" type="checkbox" />
+            <input
+              className="toggle"
+              type="checkbox"
+              checked={item.completed}
+              onChange={() => dispatch(toggle({ id: item.id }))}
+            />
             <label>{item.title}</label>
             <button className="destroy"></button>
           </div>
