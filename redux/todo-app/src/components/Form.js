6c72165151126f06f4584443894f14ed9e03fb9c
Eda Dural <edaadurall@gmail.com>
2025-06-18 12:02:43 +0300

    prepare

--- redux/todo-app/src/components/Form.js
+++ redux/todo-app/src/components/Form.js
@@ -1,7 +1,6 @@
 import { useDispatch } from "react-redux";
 import { addTodo } from "../redux/todos/todosSlice";
 import { useState } from "react";
-import { nanoid } from "@reduxjs/toolkit";
 
 function Form() {
   const dispatch = useDispatch();
@@ -11,7 +10,7 @@ function Form() {
     if (!title) return;
     e.preventDefault();
 
-    dispatch(addTodo({ id: nanoid(), title: title, completed: false }));
+    dispatch(addTodo({ title }));
     setTitle("");
   };
 
