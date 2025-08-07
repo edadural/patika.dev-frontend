d0ecc8318409f9365a58db9ebace103751f50c74
Eda Dural <edaadurall@gmail.com>
2025-08-07 13:10:40 +0300

    setup

--- redux/notes-app/app/layout.tsx
+++ redux/notes-app/app/layout.tsx
@@ -8,6 +8,7 @@ import { Providers } from "./providers";
 import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
 import { Navbar } from "@/components/navbar";
+import { GithubIcon } from "@/components/icons";
 
 export const metadata: Metadata = {
   title: {
@@ -38,25 +39,29 @@ export default function RootLayout({
       <body
         className={clsx(
           "min-h-screen text-foreground bg-background font-sans antialiased",
-          fontSans.variable,
+          fontSans.variable
         )}
       >
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
           <div className="relative flex flex-col h-screen">
             <Navbar />
+
             <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
               {children}
             </main>
-            <footer className="w-full flex items-center justify-center py-3">
+
+            <footer className="w-full flex flex-col gap-2 items-center justify-center py-5 bg-black/90">
               <Link
                 isExternal
-                className="flex items-center gap-1 text-current"
-                href="https://heroui.com?utm_source=next-app-template"
-                title="heroui.com homepage"
+                aria-label="Github"
+                href={siteConfig.links.github}
               >
-                <span className="text-default-600">Powered by</span>
-                <p className="text-primary">HeroUI</p>
+                <GithubIcon className="text-white" />
               </Link>
+
+              <p className="text-sm leading-6 text-muted-foreground text-white">
+                Copyright &copy; {new Date().getFullYear()} - Eda DURAL
+              </p>
             </footer>
           </div>
         </Providers>
