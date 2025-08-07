674213d9dd841bf74f55ac8d6453ea9e9553dc96
Eda Dural <edaadurall@gmail.com>
2025-08-07 14:31:12 +0300

    card

--- redux/notes-app/app/page.tsx
+++ redux/notes-app/app/page.tsx
@@ -1,9 +1,75 @@
+"use client";
 import Card from "@/components/card";
+import { Button } from "@heroui/button";
+import { useState } from "react";
 
 export default function Home() {
+  const colors = [
+    "secondary",
+    "warning",
+    "success",
+    "danger",
+    "primary",
+    "info",
+  ];
+  const notes = [
+    {
+      id: 1,
+      title: "Note 1",
+      description: "Description 1",
+      color: "secondary",
+    },
+    {
+      id: 2,
+      title: "Note 2",
+      description: "Description 2",
+      color: "warning",
+    },
+    {
+      id: 3,
+      title: "Note 3",
+      description: "Description 3",
+      color: "success",
+    },
+  ];
+  const [selectedColor, setSelectedColor] = useState("");
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <Card />
+      <Card
+        title={title}
+        description={description}
+        color={selectedColor}
+        isDisabled={false}
+      />
+
+      <div className="flex items-center gap-10">
+        <div className="flex gap-2">
+          {colors.map((color) => (
+            <div
+              key={color}
+              onClick={() => setSelectedColor(color)}
+              className={`w-8 h-8 rounded-full cursor-pointer bg-${color} border-2 border-gray-100 hover:scale-110 transition`}
+              title={color}
+            ></div>
+          ))}
+        </div>
+        <Button variant="bordered">Add</Button>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+        {notes.map((note) => (
+          <Card
+            key={note.id}
+            title={note.title}
+            description={note.description}
+            color={note.color}
+            isDisabled={true}
+          />
+        ))}
+      </div>
     </section>
   );
 }
--- redux/notes-app/components/card.tsx
+++ redux/notes-app/components/card.tsx
@@ -1,7 +1,25 @@
+import { Input, Textarea } from "@heroui/input";
 import React from "react";
 
-function card() {
-  return <div>card</div>;
+function card({ title, description, color, isDisabled }: any) {
+  return (
+    <div
+      className={`flex flex-col gap-3 border border-gray-200 p-4 rounded-lg bg-${color}`}
+    >
+      <Input
+        placeholder="Enter title"
+        color={color}
+        value={title}
+        isDisabled={isDisabled}
+      />
+      <Textarea
+        placeholder="Enter description"
+        color={color}
+        value={description}
+        isDisabled={isDisabled}
+      />
+    </div>
+  );
 }
 
 export default card;
